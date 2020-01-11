9f20cde952c7a6a3d6d0bfed9c831a6f18404aca
davideeparry <davideeparry@gmail.com>
2020-01-10 16:07:23 -0800

    some fixes for the postfix script

--- postfix-with-Maildir.sh
+++ postfix-with-Maildir.sh
@@ -3,4 +3,6 @@ sudo yum install postfix
 mkdir -p ~/Maildir/{cur,new,tmp}
 sudo postconf -e mailbox_command=
 sudo postconf -e home_mailbox=Maildir/
-sudo postfix reload
+sudo postconf -e inet_interfaces=all
+sudo postfix stop
+sudo postfix start
